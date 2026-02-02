831183bcbbe95ec74e4e4043bd68aaca3d5ea1db
Mohd Atif <134473011+Atif0110@users.noreply.github.com>
2026-02-02 14:53:34 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,7 @@
-# utils/helpers.py
 import pandas as pd
 
 def format_time(df):
+    df = df.copy()
     df['time'] = pd.to_datetime(df['time'])
-    df['time'] = df['time'].dt.strftime('%H:%M:%S IST')
+    df['time'] = df['time'].dt.strftime('%H:%M')
     return df
