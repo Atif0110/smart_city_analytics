54f35998906d5389e2d9bbede8a9cafbf8a5d1e8
Mohd Atif <134473011+Atif0110@users.noreply.github.com>
2026-02-02 15:52:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,10 @@ import os
 
 try:
     from dotenv import load_dotenv
-    load_dotenv() 
+    load_dotenv()
 except ImportError:
     pass
 
-OPENWEATHER_KEY = os.getenv("36e241903526674f8d73881965d8c1e1")
-TOMTOM_KEY = os.getenv("yJfFrSrspOxsP7RGfXeWxVa1GbgSDw0e")
-WAQI_KEY = os.getenv("0cf65086093d99a6ee4278f7b902d41066d7034e")
-
+OPENWEATHER_KEY = os.getenv("OPENWEATHER_KEY")
+TOMTOM_KEY = os.getenv("TOMTOM_KEY")
+WAQI_KEY = os.getenv("WAQI_KEY")
